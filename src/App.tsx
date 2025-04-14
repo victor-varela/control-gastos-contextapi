57b533004d56313db448ec3991b11e6754a9d502
Victor Varela <victor398@gmail.com>
2025-04-14 13:33:39 -0300

    feat: add styles to componet.

--- src/App.tsx
+++ src/App.tsx
@@ -14,3 +14,10 @@ function App() {
 }
 
 export default App;
+
+/*
+  En la clase el profe uso para el header la clase max-h-72 y para el div que contiene el BudgetForm la clase max-w-3xl PREGUNTAR POR QUE, que hace la diferencia??.. a la AI..
+
+  Instale tailwindcss segun la documentacion actual v-4 en adelante... 
+
+*/
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const BudgetForm = () => {
-  return <div>BudgetForm</div>;
+  const [budget, setBudget]= useState(0)
+
+  const handleChange = (e)=>{
+    console.log(e.target.value);
+    
+  }
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-center text-blue-600 text-3xl font-bold">Definir presupuesto</label>
+        <input 
+          type="number" 
+          value={0} 
+          className="w-full border-2 border-blue-700 p-2" 
+          onChange={e =>handleChange(e)}  
+        />
+      </div>
+      <input 
+        type="submit"
+        className="w-full p-2 bg-blue-600 hover:bg-blue-700 text-white font-bold text-2xl uppercase cursor-pointer"
+        value={'Definir Presupuesto'} 
+      />
+    </form>
+  );
 };
 
 export default BudgetForm;
