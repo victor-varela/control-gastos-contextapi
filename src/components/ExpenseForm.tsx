b90ea229164caf8ee8285a3c32b23b61121addc9
Victor Varela <victor398@gmail.com>
2025-05-06 21:35:59 -0300

    feat: install react-calender dependency

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,15 @@
 import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+import { useState } from "react";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const ExpenseForm = () => {
+    const [value, onChange] = useState<Value>(new Date())
   return (
     <form className="space-y-5">
       <legend className="text-2xl uppercase font-black text-center py-2 border-b-4 border-blue-500">Nuevo Gasto</legend>
@@ -33,7 +42,7 @@ const ExpenseForm = () => {
           Categoria:
         </label>
         <select name="category" id="category" className="bg-slate-100 p-2">
-          <option value=''>--- Seleccione ---</option>
+          <option value="">--- Seleccione ---</option>
           {categories.map(category => (
             <option key={category.id} value={category.id}>
               {category.name}
@@ -41,10 +50,16 @@ const ExpenseForm = () => {
           ))}
         </select>
       </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="date" className="text-xl">
+          Fecha:
+        </label>
+        <DatePicker className='bg-slate-100 p-2 border-0' onChange={onChange} value={value} />
+      </div>
       <input
         type="submit"
         className="w-full p-2 text-xl text-white uppercase bg-blue-600 font-bold cursor-pointer hover:bg-blue-500 rounded-lg"
-        value={'Registrar Gasto'}
+        value={"Registrar Gasto"}
       />
     </form>
   );
